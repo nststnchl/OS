94a933ae3be05d58ef0e3cb0868fd4f3276a7002
Anastasia Stanchul <nastyastanchul@gmail.com>
2015-03-17 22:20:05 +0300

    Helpers done

--- lib/helpers.c
+++ lib/helpers.c
@@ -20,6 +20,32 @@ ssize_t read_(int fd, void* buf, size_t count) {
 	return result;
 }
 
+ssize_t read_until(int fd, void* buf, size_t count, char delimeter) {
+    if (count == 0) {
+        return read(fd, buf, 0);
+    }
+    int good = 1;
+    int fetched = 0;
+    int result = 0;
+    int cycle = 0;
+    while (good) {
+        fetched = read(fd, buf, count);
+        if (fetched == -1) {
+            return -1;
+        }
+        for (cycle = 0; cycle < fetched; cycle++) {
+            if (((char*) buf)[cycle] == delimeter) {
+                good = 0;
+            }
+        }
+        result += fetched;
+        count -= fetched;
+        buf += fetched;
+        good = good && (count > 0) && (fetched > 0);
+    }
+    return result;
+}
+
 ssize_t write_(int fd, void* buf, size_t count) {
 	if (count == 0) {
 		return write(fd, buf, 0);
--- lib/helpers.h
+++ lib/helpers.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 
 ssize_t read_(int fd, void* buf, size_t count);
+ssize_t read_until(int fd, void* buf, size_t count, char delimeter);
 ssize_t write_(int fd, void* buf, size_t count);
 
 #endif
